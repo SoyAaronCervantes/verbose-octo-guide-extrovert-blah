21a40a0af4c617c97abca19d22ba97b2efb6c3a2
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-03-19 17:16:30 -0600

    Init commit

---
+++ app/src/main/java/com/soyaaroncervantes/diplomadoadvanced/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.soyaaroncervantes.diplomadoadvanced
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
